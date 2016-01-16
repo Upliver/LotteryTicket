534f5c68b078690c6898bbf7affd38e24aa18be9
Upliver <2639673289@qq.com>
2016-01-17 00:31:14 +0800

    模仿彩票项目--初始化项目

---
+++ LLLottery/LLLottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLLottery
+//
+//  Created by LL on 16/1/17.
+//  Copyright © 2016年 Upriver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
