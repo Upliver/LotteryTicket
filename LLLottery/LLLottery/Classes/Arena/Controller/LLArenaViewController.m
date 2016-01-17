3316eefd2b3f206f01335aaae96221fda66fd177
Upliver <2639673289@qq.com>
2016-01-18 05:11:16 +0800

    完成竞技场页面的UI搭建

--- LLLottery/LLLottery/Classes/Arena/Controller/LLArenaViewController.m
+++ LLLottery/LLLottery/Classes/Arena/Controller/LLArenaViewController.m
@@ -14,6 +14,14 @@
 
 @implementation LLArenaViewController
 
+// 控制器的View被使用到的时候会调用这个方法
+- (void)loadView
+{
+    UIImageView *iv = [[UIImageView alloc]init];
+    iv.image = [UIImage imageNamed:@"NLArenaBackground"];
+    self.view = iv;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -33,7 +41,24 @@
     [self.navigationController.navigationBar setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
     
     // 创建UISegmentControl
+    NSArray *items = @[@"足球",@"篮球"];
+    UISegmentedControl *control = [[UISegmentedControl alloc]initWithItems:items];
+    // 注意:设置navigationItem.titleView的frame时候x,y 是不起作用的
+    self.navigationItem.titleView = control;
     
+    control.frame = CGRectMake(10000, 10000, 150, 30);
+    // 设置control背景图片
+    [control setBackgroundImage:[UIImage imageNamed:@"CPArenaSegmentBG"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [control setBackgroundImage:[UIImage imageNamed:@"CPArenaSegmentSelectedBG"] forState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
+    // 设置按钮的渲染颜色
+    [control setTintColor:[UIColor colorWithRed:18/255.0 green:141/255.0 blue:141/255.0 alpha:1]];
+    // 设置按钮的选中颜色
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    dict[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    [control setTitleTextAttributes:dict forState:UIControlStateSelected];
+    
+    // 设置默认选中第一个
+    [control setSelectedSegmentIndex:0];
 }
 
 
