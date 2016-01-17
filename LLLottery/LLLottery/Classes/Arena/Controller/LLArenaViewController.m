09dff80f279454e51141f9cdc98ec760b052cc40
Upliver <2639673289@qq.com>
2016-01-18 04:54:05 +0800

    设置图片的拉伸方式,四种方式

--- LLLottery/LLLottery/Classes/Arena/Controller/LLArenaViewController.m
+++ LLLottery/LLLottery/Classes/Arena/Controller/LLArenaViewController.m
@@ -16,22 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // 1.设置导航栏的背景
+       // 设置图片的拉伸
+        //方式一
+    UIImage *image = [UIImage imageNamed:@"NLArenaNavBar64"];
+//    CGSize size = image.size;
+//    image = [image stretchableImageWithLeftCapWidth:size.width * 0.5 topCapHeight:size.height * 0.5];
+        //方式二
+        //UIEdgeInsetsMake(10, 10, 10, 10) 方法的意思是距离上左下右多少不拉伸
+//    image = [image resizableImageWithCapInsets:UIEdgeInsetsMake(size.height * 0.5, size.width * 0.5, size.height * 0.5, size.width * 0.5)];
+        //方式三
+        // 较之以上面的方法多了一个拉伸模式   平铺/拉伸
+//    image = [image resizableImageWithCapInsets:<#(UIEdgeInsets)#> resizingMode:<#(UIImageResizingMode)#>]
+        // 方式四 直接对图片进行操作
+    [self.navigationController.navigationBar setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
+    
+    // 创建UISegmentControl
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
