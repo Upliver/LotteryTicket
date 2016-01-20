76027781817f19cf550ecae94d154a03b3a47949
Upliver <2639673289@qq.com>
2016-01-20 22:47:04 +0800

    临时的一些文件提交,不需要看

--- LLLottery/LLLottery/Classes/Others/AppDelegate.m
+++ LLLottery/LLLottery/Classes/Others/AppDelegate.m
@@ -36,23 +36,21 @@
     NSLog(@"sandBoxVersion == %@",sandBoxVersion);
     // 5. 对比两个版本号
     // 6. 如果自身>本地-->显示新特性
-    if ([currentVersion compare:sandBoxVersion] == NSOrderedDescending) {
-        [defaults setValue:currentVersion forKey:@"abc"];
-        [defaults synchronize];
+//    if ([currentVersion compare:sandBoxVersion] == NSOrderedDescending) {
+//        [defaults setValue:currentVersion forKey:@"abc"];
+//        [defaults synchronize];
         // 显示新特性
         UIViewController *vc = [[UIViewController alloc]init];
         vc.view.backgroundColor  = [UIColor purpleColor];
         self.window.rootViewController = vc;
         
-    }else{
-        // 如果软件自身版本号等于沙盒 直接进入首页
-        // 2. 创建根控制器
-        LLTabBarController *tabBarVc = [[LLTabBarController alloc]init];
-        tabBarVc.view.backgroundColor = [UIColor redColor];
-         self.window.rootViewController = tabBarVc;
-        
-        
-    }
+//    }else{
+//        // 如果软件自身版本号等于沙盒 直接进入首页
+//        // 2. 创建根控制器
+//        LLTabBarController *tabBarVc = [[LLTabBarController alloc]init];
+//        tabBarVc.view.backgroundColor = [UIColor redColor];
+//         self.window.rootViewController = tabBarVc;
+//    }
     
     // 4. 显示window
     [self.window makeKeyAndVisible];
