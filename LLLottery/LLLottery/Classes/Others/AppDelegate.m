95c02cf33b8ffdbda6a1a673387602f39a61b155
Upliver <2639673289@qq.com>
2016-01-20 20:37:12 +0800

    新特性页面逻辑的实现

--- LLLottery/LLLottery/Classes/Others/AppDelegate.m
+++ LLLottery/LLLottery/Classes/Others/AppDelegate.m
@@ -23,10 +23,36 @@
     // 1. 创建window
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     
-    // 2. 创建根控制器
-    LLTabBarController *tabBarVc = [[LLTabBarController alloc]init];
-    tabBarVc.view.backgroundColor = [UIColor redColor];
-    self.window.rootViewController = tabBarVc;
+    
+    
+    
+    // 3. 获取软件自身的软件版本号
+    NSDictionary *dict = [[NSBundle mainBundle] infoDictionary];
+    NSString *currentVersion = dict[@"CFBundleShortVersionString"];
+    NSLog(@"%@",currentVersion);
+    // 4. 获取本地存储的版本号
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *sandBoxVersion = [defaults objectForKey:@"abc"];
+    NSLog(@"sandBoxVersion == %@",sandBoxVersion);
+    // 5. 对比两个版本号
+    // 6. 如果自身>本地-->显示新特性
+    if ([currentVersion compare:sandBoxVersion] == NSOrderedDescending) {
+        [defaults setValue:currentVersion forKey:@"abc"];
+        [defaults synchronize];
+        // 显示新特性
+        UIViewController *vc = [[UIViewController alloc]init];
+        vc.view.backgroundColor  = [UIColor purpleColor];
+        self.window.rootViewController = vc;
+        
+    }else{
+        // 如果软件自身版本号等于沙盒 直接进入首页
+        // 2. 创建根控制器
+        LLTabBarController *tabBarVc = [[LLTabBarController alloc]init];
+        tabBarVc.view.backgroundColor = [UIColor redColor];
+         self.window.rootViewController = tabBarVc;
+        
+        
+    }
     
     // 4. 显示window
     [self.window makeKeyAndVisible];
