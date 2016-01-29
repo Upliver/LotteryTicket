0d060dec4ecfa9d119662a3371b5a43625725cb4
Upliver <2639673289@qq.com>
2016-01-30 03:30:51 +0800

    解释了为什么修饰block变量为什么用copy,并写出来两个常见的blcok属性书写的方式,好的书写方式可以增加代码的可读性

--- LLLottery/LLLottery/Classes/Setting/Model/LLSettingItem.h
+++ LLLottery/LLLottery/Classes/Setting/Model/LLSettingItem.h
@@ -15,6 +15,7 @@ typedef enum {
 
 } LLSettingType;
 
+typedef void(^myOption)();
 
 @interface LLSettingItem : NSObject
 /**
@@ -30,6 +31,17 @@ typedef enum {
  */
 @property(nonatomic, assign) LLSettingType type;
 
+/**
+ *  定义一个名字为option的block变量
+ */
+//@property(nonatomic, copy) void (^option)();
+// 或者用下面的方式代替上面的方式,代码的可读性就会加强
+@property(nonatomic, copy) myOption option;
+
+/**
+ *  block 属性要用copy修饰  为什么? 
+ *  — 因为block只是用于保存代码,只有在调用才会执行保存的代码,但是有时有可能在调用block时,block中用到的其他的对象已经释放了,为了避免这个情况,我们一般blcok用copy修饰(本来block是存储在栈区中,copy操作会迁移到堆区中,而堆区的对象是需要我们程序员自己管理的),这样能够保证,在调用block时候,block中用到的外界对象不会被释放!
+ */
 
 /**
  *  为什么不用方法封装将来要执行的代码?
