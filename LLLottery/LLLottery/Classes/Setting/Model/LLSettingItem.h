affe5b1beacf6db05cbd069357034d3f4cc8fda2
Upliver <2639673289@qq.com>
2016-01-30 03:25:28 +0800

    解释了为什么不用方法来保存将来要执行的代码!!!

--- LLLottery/LLLottery/Classes/Setting/Model/LLSettingItem.h
+++ LLLottery/LLLottery/Classes/Setting/Model/LLSettingItem.h
@@ -7,12 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+
 typedef enum {
 
     LLSettingTypeAlert = 99,
     LLSettingTypePush
 
 } LLSettingType;
+
+
 @interface LLSettingItem : NSObject
 /**
  *  cell头像
@@ -27,5 +30,11 @@ typedef enum {
  */
 @property(nonatomic, assign) LLSettingType type;
 
+
+/**
+ *  为什么不用方法封装将来要执行的代码?
+ *  1. 有很多个cell每个cell的点击需要执行的代码不一样,如果写死,将来需求变了会很麻烦
+ *  2. 有很多个cell,点击每一行要执行的操作应该由使用者决定.
+ */
 - (void)show;
 @end
