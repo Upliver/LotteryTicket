ccf0dcf36c3cfc7090bad74325e1f44a70424440
Upliver <2639673289@qq.com>
2016-02-02 03:32:46 +0800

    设置界面 打电话,发短信,邮件分享,应用之间跳转等功能的实现

--- LLLottery/LLLottery/Classes/Setting/Controller/LLAboutViewController.m
+++ LLLottery/LLLottery/Classes/Setting/Controller/LLAboutViewController.m
@@ -28,7 +28,19 @@
 - (void)addGroup
 {
     LLSettingItem *item1 = [LLSettingItemArrow initWithImage:nil title:@"评分支持"];
+    item1.option = ^{
+        NSString *appid = @"xxxx";//每一个上架的app都有一个appid
+        NSString *urlStr = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/cn/app/id%@?mt=8",apppid];
+        [[UIApplication sharedApplication]openURL:[NSURL URLWithString:urlStr]];
+    };
     LLSettingItem *item2 = [LLSettingItemArrow initWithImage:nil title:@"热线电话"];
+    
+    item2.option = ^{
+        //拨打电话
+        NSString *teleNum1 = @"tel://18519330596";// 直接出现打电话界面
+        //NSString *teleNum2 = @"telprompt://18519330596";// 会有一个提示是否拨打这个号码!
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:teleNum1]];
+    };
     item2.subTitle = @"010-12345678";
     [self.datas addObject:@[item1,item2]];
 
--- LLLottery/LLLottery/Classes/Setting/Controller/LLShareViewController.m
+++ LLLottery/LLLottery/Classes/Setting/Controller/LLShareViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "LLShareViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface LLShareViewController ()
+@interface LLShareViewController ()<MFMessageComposeViewControllerDelegate>
 
 @end
 
@@ -25,9 +26,56 @@
 {
     LLSettingItem *item1 = [LLSettingItem initWithImage:[UIImage imageNamed:@"WeiboSina"] title:@"新浪微博"];
     LLSettingItem *item2 = [LLSettingItem initWithImage:[UIImage imageNamed:@"SmsShare"] title:@"短信分享"];
+    item2.option = ^{
+        
+//        NSString *shareNum = @"sms://10010";
+//        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:shareNum]];
+        // 以上方法有一个弊端,就是无法回退到应用程序内,因此一般不用上面方法!!!!!!!!!!
+        
+        // 开发中,一般用下面的方法
+        // 0.判断设备能不能发短信,不能就不用往下走了!
+        if (![MFMessageComposeViewController canSendText]) {
+            return ;
+        }
+        // 1.创建控制器
+        MFMessageComposeViewController *vc = [[MFMessageComposeViewController alloc]init];
+        // 2.设置短信内容
+        vc.body = @"吃饭了没?";
+        // 3.设置收件人列表
+        vc.recipients = @[@"10010",@"12306"];
+        // 4.设置代理
+        vc.messageComposeDelegate = self;
+        // 5.显示控制器
+        [self presentViewController:vc animated:YES completion:nil];
+        
+    };
     LLSettingItem *item3 = [LLSettingItem initWithImage:[UIImage imageNamed:@"MailShare"] title:@"邮件分享"];
-
+    /**
+     *  邮件分享跟短信分享差不多都是用这个库....不写了
+     */
     [self.datas addObject:@[item1,item2,item3]];
 }
 
+#pragma mark -Delegate-MFMessageComposeViewControllerDelegate
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    
+    switch (result) {
+        case MessageComposeResultCancelled://MessageComposeResultCancelled,
+            //
+            NSLog(@"取消");
+            [controller dismissViewControllerAnimated:YES completion:nil];
+            break;
+        case MessageComposeResultSent://MessageComposeResultCancelled,
+            //
+            NSLog(@"完成");
+            break;
+        case MessageComposeResultFailed://MessageComposeResultCancelled,
+            //
+            NSLog(@"发送失败");
+            break;
+        default:
+            break;
+    }
+}
 @end
