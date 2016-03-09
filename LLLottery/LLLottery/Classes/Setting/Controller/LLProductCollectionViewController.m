7aea37c9d2b88e82a62310fa1e7bf113ac204f1f
Upliver <2639673289@qq.com>
2016-03-09 23:21:37 +0800

    no message

--- LLLottery/LLLottery/Classes/Setting/Controller/LLProductCollectionViewController.m
+++ LLLottery/LLLottery/Classes/Setting/Controller/LLProductCollectionViewController.m
@@ -55,6 +55,35 @@ static NSString * const identifier = @"productItem";
     
     return cell;
 }
+/**
+ *  当选中某一个cell时候会调用
+ *
+ */
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%s",__func__);
+    
+    // 1.判断能否打开,能打开就代表已经安装---->直接打开对应的应用程序就ok了
+    // 1.1.取出对应cell的模型
+        LLProduct *product = self.products[indexPath.item];
+    // 1.2 取出对应程序头和地址,拼接成用于跳转的Url
+    NSString *urlStr = [NSString stringWithFormat:@"%@://%@",product.scheme,product.identifier];
+    NSURL *url = [NSURL URLWithString:urlStr ];
+    UIApplication *app = [UIApplication sharedApplication] ;
+    BOOL open = [app canOpenURL:url];
+    if (open) {
+        // 打开APPP
+        [app openURL:url];
+    }else{
+        // 2.没有安装--->跳转到AppStore上
+        // 从模型中取出appid
+        url = [NSURL URLWithString:product.url];
+        [app openURL:url];
+        
+    }
+    
+}
+
 
 #pragma mark -lazy
 - (NSArray *)products
